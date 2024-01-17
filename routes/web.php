dfcb7b33a8df4672de1ac56e80431cbdbd663f4a
Crow <example@example.com>
2024-01-17 10:30:34 -0500

    web.php cleaned of commets

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,8 @@
 <?php
 
-use Src\App\Controllers\SomeController;
-use Src\Http\Request;
-use Src\Http\Response;
 use Src\Routing\Router;
-use Src\Validator\Validator;
-use Src\View\View;
 
 /** @var Router */
 $router = $this;
 
-// $router->get('/', fn() => 'proof');
-
-// $router->get('/', fn() => '<a href="valid">validate data</a><br><br>' . print_r(session()->allErrors(), true));
-$router->get('/', [SomeController::class, 'index']);
-// $router->get('errors', [SomeController::class, 'err']);
-// $router->get('/', fn() => Response::redirect('home'));
-
-/*
-$router->group(['prefix' => 'assignatures'], function (Router $router) {
-    $router->group(['prefix' => '{id}'], function (Router $router) {
-        $router->get('show', fn(int $id) => "assig: #{$id}");
-    });
-});
-*/
-
-/*
-$router->get('/', function () {
-    return new View(fromBase('views/home.php'));
-});
-*/
-
-// $router->fail(function () {
-//     return new Response('Page not found', 404);
-// });
+$router->get('/', fn() => 'proof');
